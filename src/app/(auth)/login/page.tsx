62319c297c708a40f12b49fe871a5db848673dd7
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-09-24 20:56:36 +0600

    signup page data taking done

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import { BiMessage } from 'react-icons/bi'
 
-export default function Login() {
+export default function Signup() {
     return (
         <div className='min-h-screen flex items-center justify-center'>
             <div>
                 <div className='flex flex-col items-center justify-center gap-2'>
                     <div className='p-2 bg-primary size-12 flex items-center justify-center rounded-full'>
-                        <BiMessage size={24}/>
+                        <BiMessage size={24} />
                     </div>
                     <h3 className='text-2xl font-semibold'>Welcome Back!</h3>
                     <p className='text-gray-400 text-sm'>Login to your account.</p>
                 </div>
-                <fieldset className="fieldset rounded-box w-xs p-4 mt-4">
-
-                    <label className="label">Email</label>
-                    <input type="email" className="input focus:outline-none" placeholder="Email" />
-
-                    <label className="label">Password</label>
-                    <input type="password" className="input focus:outline-none" placeholder="Password" />
-
-                    <button className="btn btn-primary mt-4">Login</button>
+                <fieldset className="fieldset rounded-box w-sm p-4 mt-4 space-y-3">
+                    <div>
+                        <label className="label text-sm mb-1">Email</label>
+                        <input type="email" className="input focus:outline-none w-full" placeholder="Ex: john@gmail.com" />
+                    </div>
+                    <div>
+                        <label className="label text-sm mb-1">Password</label>
+                        <input type="password" className="input focus:outline-none w-full" placeholder="********" />
+                    </div>
+                    <button className="btn btn-primary mt-4 rounded">Login</button>
                 </fieldset>
             </div>
         </div>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,30 +1,71 @@
-import React from 'react'
+'use client'
+
+import { User } from '@/type';
+import React, { useState } from 'react'
 import { BiMessage } from 'react-icons/bi'
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 export default function Signup() {
+    const [formData, setFormData] = useState<User>({
+        fullname: '',
+        email: '',
+        password: ''
+    });
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        console.log('formdata', formData);
+    }
+
     return (
         <div className='min-h-screen flex items-center justify-center'>
             <div>
                 <div className='flex flex-col items-center justify-center gap-2'>
                     <div className='p-2 bg-primary size-12 flex items-center justify-center rounded-full'>
-                        <BiMessage size={24}/>
+                        <BiMessage size={24} />
                     </div>
                     <h3 className='text-2xl font-semibold'>Create Account</h3>
                     <p className='text-gray-400 text-sm'>Get started with your free account!</p>
                 </div>
-                <fieldset className="fieldset rounded-box w-xs p-4 mt-4">
-
-                    <label className="label">Full Name</label>
-                    <input type="fullname" className="input focus:outline-none" placeholder="Full Name" />
-
-                    <label className="label">Email</label>
-                    <input type="email" className="input focus:outline-none" placeholder="Email" />
-
-                    <label className="label">Password</label>
-                    <input type="password" className="input focus:outline-none" placeholder="Password" />
-
-                    <button className="btn btn-primary mt-4">Create Account</button>
-                </fieldset>
+                <form onSubmit={handleSubmit} className="fieldset rounded-box w-sm p-4 mt-4 space-y-3">
+                    <div>
+                        <label className="label text-sm mb-1">Full Name</label>
+                        <input
+                            type="fullname"
+                            className="input focus:outline-none w-full"
+                            placeholder="Ex: John Doe"
+                            value={formData.fullname}
+                            onChange={(e) => setFormData({ ...formData, fullname: e.target.value })}
+                        />
+                    </div>
+                    <div>
+                        <label className="label text-sm mb-1">Email</label>
+                        <input
+                            type="email"
+                            className="input focus:outline-none w-full"
+                            placeholder="Ex: john@gmail.com"
+                            value={formData.email}
+                            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                        />
+                    </div>
+                    <div>
+                        <label className="label text-sm mb-1">Password</label>
+                        <div className='relative'>
+                            {
+                                showPassword ? <FaEye size={16} className={`${formData.password === '' ? 'hidden' : 'absolute'} right-3 top-1/2 -translate-y-1/2 z-50 cursor-pointer`} onClick={() => setShowPassword((s) => !s)} title='Hide password' /> : <FaEyeSlash size={16} className={`${formData.password === '' ? 'hidden' : 'absolute'} right-3 top-1/2 -translate-y-1/2 z-50 cursor-pointer`} onClick={() => setShowPassword((s) => !s)} title='Show password' />
+                            }
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                className="input focus:outline-none w-full"
+                                placeholder="********"
+                                value={formData.password}
+                                onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                            />
+                        </div>
+                    </div>
+                    <button type='submit' className="btn btn-primary mt-4 rounded">Create Account</button>
+                </form>
             </div>
         </div>
     )
