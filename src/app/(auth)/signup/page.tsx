78fcc2063402540b82c734ffb084499aad7528cc
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-09-26 18:02:05 +0600

    account creation working normally

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -6,7 +6,10 @@ import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 import toast from 'react-hot-toast';
 import { BiMessage, BiUser } from 'react-icons/bi'
+import { CgPassword, CgSpinner } from 'react-icons/cg';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { MdEmail, MdLockOutline, MdOutlineEmail } from 'react-icons/md';
+import { TfiEmail } from 'react-icons/tfi';
 
 export default function Signup() {
     const [formData, setFormData] = useState<User>({
@@ -18,6 +21,7 @@ export default function Signup() {
     const [errorMessageFullname, setErrorMessageFullname] = useState('');
     const [errorMessageEmail, setErrorMessageEmail] = useState('');
     const [errorMessagePassword, setErrorMessagePassword] = useState('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const router = useRouter();
 
     const validateData = () => {
@@ -72,28 +76,37 @@ export default function Signup() {
 
         try {
             if (success === true) {
+
+                setIsLoading(true);
+
                 const res = await fetch('http://localhost:5001/api/auth/signup', {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
                     },
+                    credentials: "include",
                     body: JSON.stringify(formData)
                 })
+
                 const data = await res.json();
-                console.log("🚀 ~ handleSubmit ~ data:", data)
 
                 if (res.ok) {
+                    setIsLoading(false);
                     console.log("User created:", data);
                     toast.success("Account created successfully!");
                     router.push("/");
                 } else {
+                    setIsLoading(false);
                     console.error("Error:", data.message);
                     toast.error(data.message || "Signup failed!");
                 }
+
             } else {
-                toast.error('Please check your input and try again.')
+                setIsLoading(false);
+                toast.error('We couldn’t process your request due to invalid inputs. Kindly check and try again.')
             };
         } catch (error) {
+            setIsLoading(false);
             console.error("Request failed:", error);
             toast.error("Something went wrong, please try again later.");
         }
@@ -113,45 +126,49 @@ export default function Signup() {
                     <div>
                         <label className="label text-sm mb-1">Full Name</label>
                         <div className='relative'>
-                            <BiUser size={16} className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500" />
+                            <BiUser size={16} className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500 z-50" />
                             <input
                                 type="fullname"
-                                className="input focus:outline-none w-full"
-                                placeholder="Ex: John Doe"
+                                className="input focus:outline-none w-full pl-8"
+                                placeholder="John Doe"
                                 value={formData.fullname}
                                 onChange={(e) => setFormData({ ...formData, fullname: e.target.value })}
                             />
-                            <p className={`text-sm text-red-500 mt-2 ${errorMessageFullname ? 'block' : 'hidden'}`}>{errorMessageFullname && errorMessageFullname}</p>
                         </div>
+                        <p className={`text-sm text-red-500 mt-2 ${errorMessageFullname ? 'block' : 'hidden'}`}>{errorMessageFullname && errorMessageFullname}</p>
                     </div>
                     <div>
                         <label className="label text-sm mb-1">Email</label>
-                        <input
-                            type="text"
-                            className="input focus:outline-none w-full"
-                            placeholder="Ex: john@gmail.com"
-                            value={formData.email}
-                            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-                        />
+                        <div className='relative'>
+                            <MdOutlineEmail size={16} className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500 z-50" />
+                            <input
+                                type="text"
+                                className="input focus:outline-none w-full pl-8"
+                                placeholder="john@gmail.com"
+                                value={formData.email}
+                                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                            />
+                        </div>
                         <p className={`text-sm text-red-500 mt-2 ${errorMessageEmail ? 'block' : 'hidden'}`}>{errorMessageEmail && errorMessageEmail}</p>
                     </div>
                     <div>
                         <label className="label text-sm mb-1">Password</label>
                         <div className='relative'>
-                            {
-                                showPassword ? <FaEye size={16} className={`${formData.password === '' ? 'hidden' : 'absolute'} right-3 top-1/2 -translate-y-1/2 z-50 cursor-pointer`} onClick={() => setShowPassword((s) => !s)} title='Hide password' /> : <FaEyeSlash size={16} className={`${formData.password === '' ? 'hidden' : 'absolute'} right-3 top-1/2 -translate-y-1/2 z-50 cursor-pointer`} onClick={() => setShowPassword((s) => !s)} title='Show password' />
-                            }
+                            <MdLockOutline size={16} className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500 z-50" />
                             <input
                                 type={showPassword ? "text" : "password"}
-                                className="input focus:outline-none w-full"
+                                className="input focus:outline-none w-full pl-8"
                                 placeholder="********"
                                 value={formData.password}
                                 onChange={(e) => setFormData({ ...formData, password: e.target.value })}
                             />
+                            {
+                                showPassword ? <FaEye size={16} className={`${formData.password === '' ? 'hidden' : 'absolute'} right-3 top-1/2 -translate-y-1/2 z-50 cursor-pointer`} onClick={() => setShowPassword((s) => !s)} title='Hide password' /> : <FaEyeSlash size={16} className={`${formData.password === '' ? 'hidden' : 'absolute'} right-3 top-1/2 -translate-y-1/2 z-50 cursor-pointer`} onClick={() => setShowPassword((s) => !s)} title='Show password' />
+                            }
                         </div>
                         <p className={`text-sm text-red-500 mt-2 ${errorMessagePassword ? 'block' : 'hidden'}`}>{errorMessagePassword && errorMessagePassword}</p>
                     </div>
-                    <button type='submit' className="btn btn-primary mt-4 rounded">Create Account</button>
+                    <button type='submit' className="btn btn-primary mt-4 rounded">{isLoading ? <span className='flex items-center gap-x-1'><CgSpinner size={16} className="animate-spin" /> Creating account...</span> : <span>Create Account</span>}</button>
                 </form>
                 <div className='flex items-center justify-center mt-2'>
                     <p className='text-sm text-slate-300'>Already have an account? <Link href="/login" className='text-blue-400 hover:underline cursor-pointer'>Login</Link></p>
