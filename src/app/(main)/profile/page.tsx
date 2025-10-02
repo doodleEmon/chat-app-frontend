ed20bf341c4d836b75cd1b38c4fcb47452511400
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-10-02 18:42:32 +0600

    profile page updated

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -3,7 +3,7 @@
 import { RootState } from '@/redux/store'
 import Image from 'next/image'
 import React from 'react'
-import { BiUser } from 'react-icons/bi'
+import { BiCamera, BiUser } from 'react-icons/bi'
 import { FaCamera } from 'react-icons/fa'
 import { MdOutlineEmail } from 'react-icons/md'
 import { useSelector } from 'react-redux'
@@ -12,17 +12,18 @@ export default function Profile() {
   const { user } = useSelector((state: RootState) => state.auth);
   return (
     <div className='flex items-center justify-center'>
-      <div className='w-lg border p-6 mt-8'>
+      <div className='w-lg border bg-slate-800 p-6 mt-8'>
         <h3 className='text-center text-xl font-bold'>Profile</h3>
-        <p className='text-sm text-center mt-3'>Your profile information</p>
+        <p className='text-sm text-center mt-2 text-slate-400'>Your profile information</p>
         <div className='mt-8 flex justify-center'>
-          <div className='size-32 rounded-full object-cover relative'>
+          <div className='size-32 rounded-full object-cover relative p-1 border-2'>
             <Image src={user?.profilePic || '/avatar.png'} alt={user?.fullname || 'User profile picture'} height={1000} width={1000} />
-            <label className='absolute right-3 bottom-2 z-50'>
-              <FaCamera size={20} />
+            <label className='absolute right-0.5 bottom-0 z-50 bg-slate-500 p-2 rounded-full cursor-pointer'>
+              <BiCamera size={16} />
             </label>
           </div>
         </div>
+        <p className='text-sm text-slate-400 text-center mt-4'>Click the camera icon to update your photo</p>
         <div className='mt-8 space-y-4'>
           <div className='relative'>
             <BiUser size={16} className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500 z-50" />
@@ -31,7 +32,7 @@ export default function Profile() {
               className="input focus:outline-none w-full pl-8"
               placeholder="John Doe"
               value={user?.fullname}
-              // onChange={(e) => setFormData({ ...formData, fullname: e.target.value })}
+            // onChange={(e) => setFormData({ ...formData, fullname: e.target.value })}
             />
           </div>
           <div className='relative'>
@@ -41,7 +42,7 @@ export default function Profile() {
               className="input focus:outline-none w-full pl-8"
               placeholder="john@gmail.com"
               value={user?.email}
-              // onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+            // onChange={(e) => setFormData({ ...formData, email: e.target.value })}
             />
           </div>
         </div>
