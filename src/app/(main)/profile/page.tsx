7357e08ce8f900e995609f6bf56f0527b145bf09
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-10-03 23:49:01 +0600

    profile page updated

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ProfileComp from '@/componets/ProfileComp'
 
 export default function Profile() {
-
-
   return (
     <div className='flex items-center justify-center p-4 lg:p-0'>
       <ProfileComp />
