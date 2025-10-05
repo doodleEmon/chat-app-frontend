a08e3065509c3f0ba0f14b33a874d08a37e50f57
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-10-05 17:46:35 +0600

    home page initial look done

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { BiMessage } from "react-icons/bi";
 import { TiMessages } from "react-icons/ti";
 
 export default function Home() {
@@ -27,8 +28,14 @@ export default function Home() {
           }
         </div>
       </div>
-      <div className="w-[80%] h-full p-4">
-        messages area
+      <div className="w-[80%] h-full flex items-center justify-center p-4">
+        <div className="flex flex-col items-center space-y-3">
+          <div className="p-4 bg-blue-500 rounded-lg">
+            <BiMessage size={32} className="text-white" />
+          </div>
+          <p className="text-lg font-semibold">Chat with ChattyFy</p>
+          <p className="text-sm">Send and receive message without keeping your phone online.</p>
+        </div>
       </div>
     </div>
   );
